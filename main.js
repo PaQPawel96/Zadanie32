f701984cfaf399236d0b56bae946e14dac3856dd
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-17 11:41:47 +0200

    Zadanie32

---
+++ main.js
@@ -0,0 +1,14 @@
+var num1=2;
+var num2=5;
+
+function WiekszaLiczba(num1, num2)
+{
+    if(num1>num2)
+    {
+        console.log(num1);
+    }
+    else {
+        console.log(num2);
+    }
+}
+WiekszaLiczba(num1, num2);
